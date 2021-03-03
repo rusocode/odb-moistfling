97598eb10f629823145e81f27fb417c3283bbde5
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-03-03 12:08:49 -0300

    Update

--- core/src/net/mostlyoriginal/game/events/EventItemPickup.java
+++ core/src/net/mostlyoriginal/game/events/EventItemPickup.java
@@ -2,13 +2,10 @@ package net.mostlyoriginal.game.events;
 
 import net.mostlyoriginal.api.event.common.Event;
 
-/**
- * @author Daan van Yperen
- */
 public class EventItemPickup implements Event {
-    public int id;
+	public int id;
 
-    public EventItemPickup(int id) {
-        this.id = id;
-    }
+	public EventItemPickup(int id) {
+		this.id = id;
+	}
 }
--- core/src/net/mostlyoriginal/game/screen/GameScreen.java
+++ core/src/net/mostlyoriginal/game/screen/GameScreen.java
@@ -31,96 +31,60 @@ import net.mostlyoriginal.game.system.view.MyClearScreenSystem;
 import net.mostlyoriginal.plugin.OperationsPlugin;
 
 /**
- * Example main game screen.
- *
- * @author Daan van Yperen
+ * Ejemplo de pantalla de juego principal.
  */
 public class GameScreen extends TransitionableWorldScreen {
 
-    public static final String BACKGROUND_COLOR_HEX = "000000";
+	public static final String BACKGROUND_COLOR_HEX = "000000";
 
-    Class nextScreen;
+	Class nextScreen;
 
-    @Override
-    protected World createWorld() {
-        RenderBatchingSystem renderBatchingSystem;
+	// Crea el mundo del juego
+	@Override protected World createWorld() {
+		RenderBatchingSystem renderBatchingSystem;
 
-        final SpriteBatch batch = new SpriteBatch(2000);
+		final SpriteBatch batch = new SpriteBatch(2000);
 
-        WorldConfigurationBuilder worldConfigurationBuilder = new WorldConfigurationBuilder()
-                .dependsOn(
-                        MyFluidEntityPlugin.class,
-                        EntityLinkManager.class,
-                        OperationsPlugin.class,
-                        SingletonPlugin.class)
-                .with(  new EventSystem())
-                .with(
-                        new FontManager(),
-                        new TagManager(),
-                        new TiledMapManager(GameRules.nextMap)
-                )
-                .with(
-                        new CameraSystem(2),
-                        new MyClearScreenSystem(Color.valueOf(BACKGROUND_COLOR_HEX)), // probably fine.
-                        new GameScreenAssetSystem(),
-                        new MapEntitySpawnerSystem(), // Converts Tiled maps to FutureEntities to be spawned.
+		/**
+		 * Constructor de mundos.
+		 * Permite la adicion conveniente de var-arg de sistemas, administradores. Admite complementos.
+		 * */
+		WorldConfigurationBuilder worldConfigurationBuilder = new WorldConfigurationBuilder()
+				// dependsOn() especifiqua la dependencia de los sistemas/complementos
+				.dependsOn(MyFluidEntityPlugin.class, EntityLinkManager.class, OperationsPlugin.class, SingletonPlugin.class).with(new EventSystem())
+				// with() agrega los sistemas activos, se conserva el orden. Solo se permite una instancia de cada clase.
+				.with(new FontManager(), new TagManager(), new TiledMapManager(GameRules.nextMap))
+				.with(new CameraSystem(2), new MyClearScreenSystem(Color.valueOf(BACKGROUND_COLOR_HEX)), // Probablemente bien
+						new GameScreenAssetSystem(), new MapEntitySpawnerSystem(), // Convierte mapas en mosaico en FutureEntities para que se generen
 
-                        new FutureEntitySystem(new MyEntityAssemblyStrategy()), // Responsible for spawning entities.
-                        new ParticleEffectSystem(new MyParticleEffectStrategy()),
-                        new PlayerControlSystem(), // @todo phase 2: separate movement from key binding to control.
+						new FutureEntitySystem(new MyEntityAssemblyStrategy()), // Responsable de las entidades de desove
+						new ParticleEffectSystem(new MyParticleEffectStrategy()), new PlayerControlSystem(),
+						// @todo fase 2: movimiento separado del enlace de teclas al control
 
-                        new PhysicsSystem(), // for particles.
-                        new BoxPhysicsSystem(),
-                        new BoxPhysicsMouseSystem(),
-                        new LatchingSystem(),
-                        new MouseCursorSystem(),
+						new PhysicsSystem(), // Para particulas
+						new BoxPhysicsSystem(), new BoxPhysicsMouseSystem(), new LatchingSystem(), new MouseCursorSystem(),
 
+						new PickupSystem(), new LeakSystem(), new BeamedSystem(),
 
-                        new PickupSystem(),
-                        new LeakSystem(),
-                        new BeamedSystem(),
+						new LevelTimerSystem(),
 
-                        new LevelTimerSystem(),
+						new CameraFollowSystem(), new PlayerAnimationSystem(),
 
-                        new CameraFollowSystem(),
-                        new PlayerAnimationSystem(),
+						new BreathingSfxSystem(), new LeakSfxSystem(),
 
-                        new BreathingSfxSystem(),
-                        new LeakSfxSystem(),
+						new RenderBackgroundSystem(),
 
-                        new RenderBackgroundSystem(),
+						renderBatchingSystem = new RenderBatchingSystem(), new MyAnimRenderSystem(renderBatchingSystem),
+						new MyLabelRenderSystem(renderBatchingSystem), new MapLayerRenderSystem(renderBatchingSystem, batch), new UiSystem(),
+						new SoundPlaySystem("leak-loop.wav", "oxygen-recharge-1", "oxygen-recharge-2", "astronaut-pops", "breath-normal",
+								"breath-laboured", "breath-suffocating", "suit-puncture", "suit-almost-puncture", "suit-last-oxygen-escapes",
+								"ship-reached", "tractor-lock-1.wav", "tractor-lock-2.wav", "tractor-unlock.wav", "orb-on", "randomise"),
+						new BoxPhysicsDebugRenderSystem(), new TransitionSystem(GdxArtemisGame.getInstance(), this));
 
-                        renderBatchingSystem = new RenderBatchingSystem(),
-                        new MyAnimRenderSystem(renderBatchingSystem),
-                        new MyLabelRenderSystem(renderBatchingSystem),
-                        new MapLayerRenderSystem(renderBatchingSystem, batch),
-                        new UiSystem(),
-                        new SoundPlaySystem(
-                                "leak-loop.wav",
-                                "oxygen-recharge-1",
-                                "oxygen-recharge-2",
-                                "astronaut-pops",
-                                "breath-normal",
-                                "breath-laboured",
-                                "breath-suffocating",
-                                "suit-puncture",
-                                "suit-almost-puncture",
-                                "suit-last-oxygen-escapes",
-                                "ship-reached",
-                                "tractor-lock-1.wav",
-                                "tractor-lock-2.wav",
-                                "tractor-unlock.wav",
-                                "orb-on",
-                                "randomise"
-                        ),
-                        new BoxPhysicsDebugRenderSystem(),
-                        new TransitionSystem(GdxArtemisGame.getInstance(), this)
-                );
+		if (GameRules.DEBUG_ENABLED) {
+			// worldConfigurationBuilder.with(new DebugOptionControlSystem());
+		}
 
-        if (GameRules.DEBUG_ENABLED) {
-//            worldConfigurationBuilder.with(new DebugOptionControlSystem());
-        }
-
-        return new World(worldConfigurationBuilder.build());
-    }
+		return new World(worldConfigurationBuilder.build());
+	}
 }
